d97e033a1ddd629f83c01cf4b623a9041300bccf
martin <martin-cleven@gmx.net>
2020-10-29 12:36:04 +0100

    Basic execution working

--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,13 @@
 import numpy as np
 
 
-def handler(event, context):
-    return
+def random_handler(event: dict, context: dict) -> dict:
+    lower = event['lower']
+    upper = event['upper']
+    a = np.random.randint(lower, upper)
+    return {'result': a}
+
+
+if __name__ == '__main__':
+    x = random_handler({'upper': 3, 'lower': 1}, {})
+    print(x['result'])
